2e7a7dc133905ab535eee78cb52225b81b9ddb61
jlfudeveloper <69946005+jlfudeveloper@users.noreply.github.com>
2020-09-23 16:59:49 -0700

    input and prnt board done

--- cpptictactoe.cpp
+++ cpptictactoe.cpp
@@ -1,3 +1,4 @@
+File Edit Options Buffers Tools C++ Help
 #include <iostream>
 
 using namespace std;
@@ -7,18 +8,18 @@ bool checkWin(char board[3][3]);
 
 void printBoard(char board[3][3]);
 int main(){
-  
+
   char board[3][3];
   char move[3];
   bool p1turn = true;
   for(int a = 0; a < 3; a++){
     for(int b = 0; b < 3; b++){
-      board[a][b] = ' '; 
+      board[a][b] = ' ';
     }
   }
   printBoard(board);
   cout << "welcome to tictactoe! input your moves in the format a0, b1, etc." << endl;
-  
+
   while(true){
     cout << "input your move: " << endl;
     cin.get(move,3);
@@ -40,9 +41,8 @@ bool legalMove(char board[3][3], char marker[2]){
 
 bool checkWin(char board[3][3]){
   //check for wins or ties
-  return true; 
+  return true;
 }
-
 //char [3][3] placeMarker(char board[3][3], char marker[2]){
   //place the marker
 //}
@@ -59,3 +59,4 @@ void printBoard(char board[3][3]){
     cout << line << endl;
   }
 }
+
